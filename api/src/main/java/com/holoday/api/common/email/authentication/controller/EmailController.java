3253d26afce19c138a96449b829f589d71ac9a12
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-27 18:44:19 +0900

    complete email authentication

--- api/src/main/java/com/holoday/api/common/email/authentication/controller/EmailController.java
+++ api/src/main/java/com/holoday/api/common/email/authentication/controller/EmailController.java
@@ -1,4 +1,34 @@
 package com.holoday.api.common.email.authentication.controller;
 
+import com.holoday.api.common.email.authentication.dto.EmailDTO;
+import com.holoday.api.common.email.authentication.service.EmailService;
+import com.holoday.api.common.responsedto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/holoday")
+@RequiredArgsConstructor
 public class EmailController {
+    private final EmailService emailService;
+
+    @PostMapping("/send")
+    public ResponseEntity<ApiResponse<Void>> sendEmail(@RequestBody EmailDTO.EmailSendRequest request){
+        emailService.sendEmail(request);
+        return ResponseEntity.ok(ApiResponse.success("인증번호가 발송되었습니다."));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<ApiResponse<Void>> verifyCode(@RequestBody EmailDTO.EmailAuthenticationRequest request){
+        boolean isSuccess = emailService.verifyCode(request);
+        if (isSuccess){
+            return ResponseEntity.ok(ApiResponse.success("인증 성공!"));
+        } else{
+         return ResponseEntity.badRequest().body(ApiResponse.fail("인증에 실패했습니다."));
+        }
+    }
 }
--- api/src/main/java/com/holoday/api/common/email/authentication/service/EmailService.java
+++ api/src/main/java/com/holoday/api/common/email/authentication/service/EmailService.java
@@ -1,4 +1,8 @@
 package com.holoday.api.common.email.authentication.service;
 
+import com.holoday.api.common.email.authentication.dto.EmailDTO;
+
 public interface EmailService {
+    void sendEmail(EmailDTO.EmailSendRequest request);
+    boolean verifyCode(EmailDTO.EmailAuthenticationRequest request);
 }
