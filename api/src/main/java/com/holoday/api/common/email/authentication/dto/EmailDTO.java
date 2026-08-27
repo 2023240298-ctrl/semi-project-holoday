b2d6b82cb80f4157f7f1457f5c9667d459120c5c
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-27 17:51:36 +0900

    add api response class

--- api/src/main/java/com/holoday/api/common/email/authentication/dto/EmailDTO.java
+++ api/src/main/java/com/holoday/api/common/email/authentication/dto/EmailDTO.java
@@ -1,4 +1,21 @@
 package com.holoday.api.common.email.authentication.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class EmailDTO {
+
+    @Getter
+    @NoArgsConstructor
+    public static class EmailSendRequest{
+        private String email;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class EmailAuthenticationRequest{
+        private String email;
+        private String code;
+    }
+
 }
---
+++ api/src/main/java/com/holoday/api/common/responsedto/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.holoday.api.common.responsedto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<Void> success(String message){
+        return new ApiResponse<>(true, message, null);
+    }
+
+    public static <T> ApiResponse success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<Void> fail(String message){
+        return new ApiResponse<>(false, message, null);
+    }
+}
