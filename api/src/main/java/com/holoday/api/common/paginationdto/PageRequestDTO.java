764d163f9d5da3f00e6ac3bf9d36cbe4814cae61
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-27 12:41:24 +0900

    add pagination dto

---
+++ api/src/main/java/com/holoday/api/common/paginationdto/PageRequestDTO.java
@@ -0,0 +1,30 @@
+package com.holoday.api.common.paginationdto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class PageRequestDTO {
+    private int page = 1;
+    private int size = 10;
+
+    private String searchType = "";
+    private String keyword;
+
+    public PageRequestDTO(int page, int size) {
+        this.page = page;
+        this.size = size;
+    }
+
+    public Pageable getPageable(String sortField) {
+        return PageRequest.of(page - 1, size, Sort.Direction.DESC, sortField);
+    }
+}
