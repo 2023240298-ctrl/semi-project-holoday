208e3c39fe6afb9f12d6fd5195b5e0876e7e8a57
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-27 11:42:14 +0900

    스프링 패키지 생성

---
+++ api/src/test/java/com/holoday/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holoday.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
