645f6c9835a7e915a83366d722e6640f3a39b997
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-27 15:43:22 +0900

    add user entity, dto

--- api/src/main/java/com/holoday/api/user/dto/UserDTO.java
+++ api/src/main/java/com/holoday/api/user/dto/UserDTO.java
@@ -1,4 +1,28 @@
 package com.holoday.api.user.dto;
 
-public class UserDTO {
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public class UserDTO extends User {
+    private String userId;
+    private String userEmail;
+    private String userNick;
+    private String userPw;
+    private boolean userIsAdmin;
+
+    public UserDTO(String userId, String userEmail, String userNick, String userPw, boolean userIsAdmin) {
+        super(userId, userPw,
+                List.of(new SimpleGrantedAuthority(userIsAdmin ? "ROLE_ADMIN" : "ROLE_USER")));
+        this.userId = userId;
+        this.userEmail = userEmail;
+        this.userNick = userNick;
+        this.userPw = userPw;
+        this.userIsAdmin = userIsAdmin;
+    }
 }
--- api/src/main/java/com/holoday/api/user/entity/User.java
+++ api/src/main/java/com/holoday/api/user/entity/User.java
@@ -1,4 +1,33 @@
 package com.holoday.api.user.entity;
 
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.type.NumericBooleanConverter;
+
+@Entity
+@Table(name = "h_user")
+@Getter
+@NoArgsConstructor
 public class User {
+    @Id
+    private String userId;
+    private String userEmail;
+    private String userNick;
+    private String userPw;
+    @Convert(converter = NumericBooleanConverter.class)
+    private boolean userIsAdmin;
+
+    public User(String userId, String userEmail, String userNick, String userPw, boolean userIsAdmin) {
+        this.userId = userId;
+        this.userEmail = userEmail;
+        this.userNick = userNick;
+        this.userPw = userPw;
+        this.userIsAdmin = userIsAdmin;
+    }
+
+
 }
