6e87546281fec81226651750f3caf85dd990e34a
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-31 19:31:58 +0900

    작업 장소 이동

--- holoday/src/pages/SignUpPage.jsx
+++ holoday/src/pages/SignUpPage.jsx
@@ -1,1 +1,14 @@
-.
+import SignUpComponent from "../components/SignUpComponent";
+
+const SignUpPage = () => {
+
+
+    return (
+        <>
+            <div>
+                <h1>회원 가입 페이지</h1>
+            </div>
+        </>
+    );
+};
+export default SignUpPage;
--- holoday/src/router/memberRouter.jsx
+++ holoday/src/router/memberRouter.jsx
@@ -4,8 +4,16 @@ const memberRouter = () => {
          path: "login",
          HydrateFallback: () => <div>로그인 페이지 불러오는 중</div>,
          lazy: async () => {
-            const {default: Component} = await import("../pages/LoginPage");
-            return {Component};
+            const { default: Component } = await import("../pages/LoginPage");
+            return { Component };
+         },
+      },
+      {
+         path: "holosignup",
+         HydrateFallback: () => <div>회원가입 페이지 불러오는 중</div>,
+         lazy: async () => {
+            const { default: Component } = await import("../pages/SignUpPage");
+            return { Component };
          },
       },
    ];
