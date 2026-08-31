ca0cdfa7b6152276e4932d709f21467e77441110
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-31 19:19:04 +0900

    오류 수정

--- holoday/src/components/common/Header.jsx
+++ holoday/src/components/common/Header.jsx
@@ -12,55 +12,44 @@ const Header = ({ isLogin, setIsLogin }) => {
   };
 
   return (
-const Header = ({ isLogin, setIsLogin }) => {
-      const navigate = useNavigate();
+    <header className="header">
+      <div className="header_logo logo-text ">
+        HOLODAY
+      </div>
 
-      const handleLogout = () => {
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
-        setIsLogin(false);
-        navigate("/");
-      };
+      <nav className="header_menu head-text">
+        <a href="/">홈</a>
+        <a href="/info">홀로정보</a>
+        <a href="/board">홀로라운지</a>
+        <a href="/card">홀로땜</a>
+      </nav>
 
-      return (
-        <header className="header">
-          <div className="header_logo logo-text ">
-            HOLODAY
-          </div>
+      <div className="header_login head-text">
 
-          <nav className="header_menu head-text">
-            <a href="/">홈</a>
-            <a href="/info">홀로정보</a>
-            <a href="/board">홀로라운지</a>
-            <a href="/card">홀로땜</a>
-          </nav>
+        {isLogin ? (
+          <button
+            type="button"
+            onClick={handleLogout}
+          >
+            로그아웃
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => navigate("/login")}
+          >
+            로그인
+          </button>
+        )}
+        <button
+          type="button"
+        >
+          회원가입
+        </button>
+      </div>
+    </header>
+  );
 
-          <div className="header_login head-text">
-
-            {isLogin ? (
-              <button
-                type="button"
-                onClick={handleLogout}
-              >
-                로그아웃
-              </button>
-            ) : (
-              <button
-                type="button"
-                onClick={() => navigate("/login")}
-              >
-                로그인
-              </button>
-            )}
-            <button
-              type="button"
-            >
-              회원가입
-            </button>
-          </div>
-        </header>
-      );
-
-    }
+}
 
 export default Header;
