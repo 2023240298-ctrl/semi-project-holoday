492596e3b9db677ddcbc3ac36bab54d06a1f5139
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-08-31 12:29:04 +0900

    add admin user

--- api/src/test/java/com/holoday/api/UserRepositoryTests.java
+++ api/src/test/java/com/holoday/api/UserRepositoryTests.java
@@ -38,6 +38,21 @@ public class UserRepositoryTests {
         printUser(result);
     }
 
+
+    @Test
+    @Commit
+    public void testInsertUserAdmin() {
+        User user = new User(
+                "admin1234",
+                "gildong11@naver.com",
+                "홍길동",
+                passwordEncoder.encode("1234"),
+                true
+        );
+
+        User result = userRepository.save(user);
+    }
+
     @Test
     public void testRead() {
         String userId = "alstn11";
