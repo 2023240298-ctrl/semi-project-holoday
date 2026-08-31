bb3e8922f00908ba0955799f080e6267fca77adc
AnaisKL <anaiskl0618@gmail.com>
2026-08-31 11:06:53 +0900

    merge 할 때 수정할 임시 App과 main 페이지

--- holoday/src/main.jsx
+++ holoday/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,5 +6,27 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+  </StrictMode>,
+)*/
+
+// 내 게시판 연결하기 위한 코드(merge 할 때 삭제 예정)
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import holoBoardRouter from './holoLounge/holoBoardRouter.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: holoBoardRouter(),
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
