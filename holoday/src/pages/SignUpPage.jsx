3bc34a1bd03056b314cc067e47e6286fa971270e
2023240298-ctrl <2023240298@yonsei.ac.kr>
2026-09-03 09:53:56 +0900

    배경색 수정중

--- holoday/src/pages/SignUpPage.jsx
+++ holoday/src/pages/SignUpPage.jsx
@@ -4,7 +4,7 @@ import './SignUpPage.css';
 const SignUpPage = () => {
     return (
         <>
-            <div className="select-none flex flex-col items-center justify-start min-h-screen pt-12 total-container">
+            <div className="bg-white w-full select-none flex flex-col items-center justify-start min-h-screen pt-12 total-container">
                 <h1 className="logo-text text-4xl signup-head-text">회원 가입 페이지</h1>
                 <SignUpComponent />
             </div>
